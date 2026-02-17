f0304a5840c7d08f8a72c88bfe0bc16bf274f969
Hardik <hardiksoni252007@gmail.com>
2026-02-17 23:08:13 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,33 @@
+// const express = require('express');
+
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.routes.js";
+
+
+dotenv.config();
+
+const app = express();
+
+const __dirname = path.resolve();
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+// make ready for deployment
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*", (_, res)=>{
+        res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"));
+    })
+}
+
+
+app.listen(PORT, ()=> console.log(" server is running on port: "+PORT));
